1761b86db29585865750f85bce54c815f5d9c2d5
Dmitry Sosnov <dmitry.sosnov@cern.ch>
2018-11-12 16:08:25 +0100

    Integrated ZDC RecHit Producer from Quan

--- HeavyIonsAnalysis/JetAnalysis/test/runForestAOD_pponAA_MB_103X.py
+++ HeavyIonsAnalysis/JetAnalysis/test/runForestAOD_pponAA_MB_103X.py
@@ -164,9 +164,18 @@ process.CSVscikitTags.weightFile = cms.FileInPath(
 ###############################################################################
 
 #########################
+# ZDC RecHit Producer
+#########################
+process.load('RecoHI.ZDCRecHit.QWZDC2018Producer_cfi')
+process.load('RecoHI.ZDCRecHit.QWZDC2018RecHit_cfi')
+
+###############################################################################
+
+#########################
 # RecHits & pfTowers (HF, Castor & ZDC)
 #########################
 process.load('HeavyIonsAnalysis.JetAnalysis.rechitanalyzer_cfi')
+process.rechitanalyzerpp.zdcRecHitSrc = cms.untracked.InputTag("QWzdcreco")
 
 ###############################################################################
 
@@ -189,6 +198,8 @@ process.ana_step = cms.Path(
     process.pfcandAnalyzer +
     process.pfcandAnalyzerCS +
     process.trackSequencesPP +
+    process.zdcdigi +
+    process.QWzdcreco +
     process.rechitanalyzerpp
     )
 
