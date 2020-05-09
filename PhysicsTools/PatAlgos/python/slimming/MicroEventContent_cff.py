6951a486f62f2f0b6ae449969d7d177c3a5f7350
Ran Bi <biran@mit.edu>
2020-05-09 04:30:24 +0200

    remove all ak8/puppi related modules

--- PhysicsTools/PatAlgos/python/slimming/MicroEventContent_cff.py
+++ PhysicsTools/PatAlgos/python/slimming/MicroEventContent_cff.py
@@ -155,8 +155,18 @@ _pp_on_AA_2018_extraCommands = [
     'keep CrossingFramePlaybackInfoNew_mix_*_*',
     'keep booledmValueMap_*MuonID_*_*',
     'keep patPackedCandidates_hipixeltracks_*_*',
-    #'keep *_heavyIon_*_*',
+    'drop *_slimmedJetsAK8_*_*',
+    'drop *_slimmedJetsAK8PFPuppiSoftDropPacked_SubJets_*',
+    'drop *_slimmedJetsPuppi_*_*',
+    'drop *_slimmedMETsPuppi_*_*',
 ]
+
+_pp_on_AA_2018_extraCommandsGEN = [
+    'keep *_heavyIon_*_*',
+    'drop *_slimmedGenJetsAK8__*',
+    'drop *_slimmedGenJetsAK8SoftDropSubJets__*',
+]
+
 from Configuration.Eras.Modifier_pp_on_AA_2018_cff import pp_on_AA_2018
 pp_on_AA_2018.toModify(MicroEventContent, outputCommands = MicroEventContent.outputCommands + _pp_on_AA_2018_extraCommands)
-pp_on_AA_2018.toModify(MicroEventContentMC, outputCommands = MicroEventContentMC.outputCommands + _pp_on_AA_2018_extraCommands + ['keep *_heavyIon_*_*'])
+pp_on_AA_2018.toModify(MicroEventContentMC, outputCommands = MicroEventContentMC.outputCommands + _pp_on_AA_2018_extraCommands + _pp_on_AA_2018_extraCommandsGEN)
