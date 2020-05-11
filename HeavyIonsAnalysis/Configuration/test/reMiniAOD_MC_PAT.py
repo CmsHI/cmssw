b9b7fd89d0fafdcc8f58d7c7f6842416fc5b634b
ddesouza <dener.lemos@cern.ch>
2020-05-12 00:30:47 +0200

    Including the quality event filters in miniAOD

--- HeavyIonsAnalysis/Configuration/test/reMiniAOD_MC_PAT.py
+++ HeavyIonsAnalysis/Configuration/test/reMiniAOD_MC_PAT.py
@@ -21,6 +21,9 @@ process.load('PhysicsTools.PatAlgos.slimming.metFilterPaths_cff')
 process.load('Configuration.StandardSequences.PATMC_cff')
 process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+process.load('HeavyIonsAnalysis.Configuration.hfCoincFilter_cff') #add centrality filter cfg file
+
+
 
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(-1)
@@ -148,11 +151,98 @@ process.Flag_trkPOG_manystripclus53X = cms.Path(~process.manystripclus53X)
 process.Flag_BadPFMuonSummer16Filter = cms.Path(process.BadPFMuonSummer16Filter)
 process.Flag_muonBadTrackFilter = cms.Path(process.muonBadTrackFilter)
 process.Flag_CSCTightHalo2015Filter = cms.Path(process.CSCTightHalo2015Filter)
+#---------------add centrality filters here---------------------
+# make calotowers into candidates
+process.Flag_towersAboveThreshold = cms.Path(process.towersAboveThreshold)
+# make calotowers into candidates with threshold 2, 4 and 5
+process.Flag_towersAboveThresholdTh2 = cms.Path(process.towersAboveThresholdTh2)
+process.Flag_towersAboveThresholdTh4 = cms.Path(process.towersAboveThresholdTh4)
+process.Flag_towersAboveThresholdTh5 = cms.Path(process.towersAboveThresholdTh5)
+# select HF+ towers above threshold
+process.Flag_hfPosTowers = cms.Path(process.hfPosTowers)
+# select HF- towers above threshold
+process.Flag_hfNegTowers = cms.Path(process.hfNegTowers)
+# select HF+/HF- towers above threshold 2, 4 and 5
+process.Flag_hfPosTowersTh2 = cms.Path(process.hfPosTowersTh2)
+process.Flag_hfNegTowersTh2 = cms.Path(process.hfNegTowersTh2)
+process.Flag_hfPosTowersTh4 = cms.Path(process.hfPosTowersTh4)
+process.Flag_hfNegTowersTh4 = cms.Path(process.hfNegTowersTh4)
+process.Flag_hfPosTowersTh5 = cms.Path(process.hfPosTowersTh5)
+process.Flag_hfNegTowersTh5 = cms.Path(process.hfNegTowersTh5)
+# require at least one HF+ tower above threshold
+process.Flag_hfPosFilter = cms.Path(process.hfPosFilter)
+process.Flag_hfNegFilter = cms.Path(process.hfNegFilter)
+# require at least one HF+/HF- tower above threshold 2, 4and 5
+process.Flag_hfPosFilterTh2 = cms.Path(process.hfPosFilterTh2)
+process.Flag_hfNegFilterTh2 = cms.Path(process.hfNegFilterTh2)
+process.Flag_hfPosFilterTh4 = cms.Path(process.hfPosFilterTh4)
+process.Flag_hfNegFilterTh4 = cms.Path(process.hfNegFilterTh4)
+process.Flag_hfPosFilterTh5 = cms.Path(process.hfPosFilterTh5)
+process.Flag_hfNegFilterTh5 = cms.Path(process.hfNegFilterTh5)
+# one HF tower above threshold on each side
+process.Flag_hfCoincFilterTh2 = cms.Path(process.hfCoincFilterTh2)
+process.Flag_hfCoincFilterTh3 = cms.Path(process.hfCoincFilterTh3)
+process.Flag_hfCoincFilterTh4 = cms.Path(process.hfCoincFilterTh4)
+process.Flag_hfCoincFilterTh5 = cms.Path(process.hfCoincFilterTh5)
+# two HF towers above threshold on each side
+process.Flag_hfPosFilter2 = cms.Path(process.hfPosFilter2)
+process.Flag_hfNegFilter2 = cms.Path(process.hfNegFilter2)
+process.Flag_hfPosFilter2Th2 = cms.Path(process.hfPosFilter2Th2)
+process.Flag_hfNegFilter2Th2 = cms.Path(process.hfNegFilter2Th2)
+process.Flag_hfPosFilter2Th4 = cms.Path(process.hfPosFilter2Th4)
+process.Flag_hfNegFilter2Th4 = cms.Path(process.hfNegFilter2Th4)
+process.Flag_hfPosFilter2Th5 = cms.Path(process.hfPosFilter2Th5)
+process.Flag_hfNegFilter2Th5 = cms.Path(process.hfNegFilter2Th5)
+process.Flag_hfCoincFilter2Th2 = cms.Path(process.hfCoincFilter2Th2)
+process.Flag_hfCoincFilter2Th3 = cms.Path(process.hfCoincFilter2Th3)
+process.Flag_hfCoincFilter2Th4 = cms.Path(process.hfCoincFilter2Th4)
+process.Flag_hfCoincFilter2Th5 = cms.Path(process.hfCoincFilter2Th5)
+#three HF towers above threshold on each side
+process.Flag_hfPosFilter3 = cms.Path(process.hfPosFilter3)
+process.Flag_hfNegFilter3 = cms.Path(process.hfNegFilter3)
+process.Flag_hfPosFilter3Th2 = cms.Path(process.hfPosFilter3Th2)
+process.Flag_hfNegFilter3Th2 = cms.Path(process.hfNegFilter3Th2)
+process.Flag_hfPosFilter3Th4 = cms.Path(process.hfPosFilter3Th4)
+process.Flag_hfNegFilter3Th4 = cms.Path(process.hfNegFilter3Th4)
+process.Flag_hfPosFilter3Th5 = cms.Path(process.hfPosFilter3Th5)
+process.Flag_hfNegFilter3Th5 = cms.Path(process.hfNegFilter3Th5)
+process.Flag_hfCoincFilter3Th2 = cms.Path(process.hfCoincFilter3Th2)
+process.Flag_hfCoincFilter3Th3 = cms.Path(process.hfCoincFilter3Th3)
+process.Flag_hfCoincFilter3Th4 = cms.Path(process.hfCoincFilter3Th4)
+process.Flag_hfCoincFilter3Th5 = cms.Path(process.hfCoincFilter3Th5)
+#four HF towers above threshold on each side
+process.Flag_hfPosFilter4 = cms.Path(process.hfPosFilter4)
+process.Flag_hfNegFilter4 = cms.Path(process.hfNegFilter4)
+process.Flag_hfPosFilter4Th2 = cms.Path(process.hfPosFilter4Th2)
+process.Flag_hfNegFilter4Th2 = cms.Path(process.hfNegFilter4Th2)
+process.Flag_hfPosFilter4Th4 = cms.Path(process.hfPosFilter4Th4)
+process.Flag_hfNegFilter4Th4 = cms.Path(process.hfNegFilter4Th4)
+process.Flag_hfPosFilter4Th5 = cms.Path(process.hfPosFilter4Th5)
+process.Flag_hfNegFilter4Th5 = cms.Path(process.hfNegFilter4Th5)
+process.Flag_hfCoincFilter4Th2 = cms.Path(process.hfCoincFilter4Th2)
+process.Flag_hfCoincFilter4Th3 = cms.Path(process.hfCoincFilter4Th3)
+process.Flag_hfCoincFilter4Th4 = cms.Path(process.hfCoincFilter4Th4)
+process.Flag_hfCoincFilter4Th5 = cms.Path(process.hfCoincFilter4Th5)
+#five hf towers above threshold on each side
+process.Flag_hfPosFilter5 = cms.Path(process.hfPosFilter5)
+process.Flag_hfNegFilter5 = cms.Path(process.hfNegFilter5)
+process.Flag_hfPosFilter5Th2 = cms.Path(process.hfPosFilter5Th2)
+process.Flag_hfNegFilter5Th2 = cms.Path(process.hfNegFilter5Th2)
+process.Flag_hfPosFilter5Th4 = cms.Path(process.hfPosFilter5Th4)
+process.Flag_hfNegFilter5Th4 = cms.Path(process.hfNegFilter5Th4)
+process.Flag_hfPosFilter5Th5 = cms.Path(process.hfPosFilter5Th5)
+process.Flag_hfNegFilter5Th5 = cms.Path(process.hfNegFilter5Th5)
+process.Flag_hfCoincFilter5Th2 = cms.Path(process.hfCoincFilter5Th2)
+process.Flag_hfCoincFilter5Th3 = cms.Path(process.hfCoincFilter5Th3)
+process.Flag_hfCoincFilter5Th4 = cms.Path(process.hfCoincFilter5Th4)
+process.Flag_hfCoincFilter5Th5 = cms.Path(process.hfCoincFilter5Th5)
+#---------------------------------------------------------------
 process.endjob_step = cms.EndPath(process.endOfProcess)
 process.MINIAODSIMoutput_step = cms.EndPath(process.MINIAODSIMoutput)
 
 # Schedule definition
-process.schedule = cms.Schedule(process.Flag_HBHENoiseFilter,process.Flag_HBHENoiseIsoFilter,process.Flag_CSCTightHaloFilter,process.Flag_CSCTightHaloTrkMuUnvetoFilter,process.Flag_CSCTightHalo2015Filter,process.Flag_globalTightHalo2016Filter,process.Flag_globalSuperTightHalo2016Filter,process.Flag_HcalStripHaloFilter,process.Flag_hcalLaserEventFilter,process.Flag_EcalDeadCellTriggerPrimitiveFilter,process.Flag_EcalDeadCellBoundaryEnergyFilter,process.Flag_ecalBadCalibFilter,process.Flag_goodVertices,process.Flag_eeBadScFilter,process.Flag_ecalLaserCorrFilter,process.Flag_trkPOGFilters,process.Flag_chargedHadronTrackResolutionFilter,process.Flag_muonBadTrackFilter,process.Flag_BadChargedCandidateFilter,process.Flag_BadPFMuonFilter,process.Flag_BadChargedCandidateSummer16Filter,process.Flag_BadPFMuonSummer16Filter,process.Flag_trkPOG_manystripclus53X,process.Flag_trkPOG_toomanystripclus53X,process.Flag_trkPOG_logErrorTooManyClusters,process.Flag_METFilters,process.endjob_step,process.MINIAODSIMoutput_step)
+process.schedule = cms.Schedule(process.Flag_HBHENoiseFilter,process.Flag_HBHENoiseIsoFilter,process.Flag_CSCTightHaloFilter,process.Flag_CSCTightHaloTrkMuUnvetoFilter,process.Flag_CSCTightHalo2015Filter,process.Flag_globalTightHalo2016Filter,process.Flag_globalSuperTightHalo2016Filter,process.Flag_HcalStripHaloFilter,process.Flag_hcalLaserEventFilter,process.Flag_EcalDeadCellTriggerPrimitiveFilter,process.Flag_EcalDeadCellBoundaryEnergyFilter,process.Flag_ecalBadCalibFilter,process.Flag_goodVertices,process.Flag_eeBadScFilter,process.Flag_ecalLaserCorrFilter,process.Flag_trkPOGFilters,process.Flag_chargedHadronTrackResolutionFilter,process.Flag_muonBadTrackFilter,process.Flag_BadChargedCandidateFilter,process.Flag_BadPFMuonFilter,process.Flag_BadChargedCandidateSummer16Filter,process.Flag_BadPFMuonSummer16Filter,process.Flag_trkPOG_manystripclus53X,process.Flag_trkPOG_toomanystripclus53X,process.Flag_trkPOG_logErrorTooManyClusters,process.Flag_METFilters,process.Flag_towersAboveThreshold,process.Flag_towersAboveThresholdTh2,process.Flag_towersAboveThresholdTh4,process.Flag_towersAboveThresholdTh5,process.Flag_hfPosTowers,process.Flag_hfNegTowers,process.Flag_hfPosTowersTh2,process.Flag_hfNegTowersTh2,process.Flag_hfPosTowersTh4,process.Flag_hfNegTowersTh4,process.Flag_hfPosTowersTh5,process.Flag_hfNegTowersTh5,process.Flag_hfPosFilter,process.Flag_hfNegFilter,process.Flag_hfPosFilterTh2,process.Flag_hfNegFilterTh2,process.Flag_hfPosFilterTh4,process.Flag_hfNegFilterTh4,process.Flag_hfPosFilterTh5,process.Flag_hfNegFilterTh5,process.Flag_hfCoincFilterTh2,process.Flag_hfCoincFilterTh3,process.Flag_hfCoincFilterTh4,process.Flag_hfCoincFilterTh5,process.Flag_hfPosFilter2,process.Flag_hfNegFilter2,process.Flag_hfPosFilter2Th2,process.Flag_hfNegFilter2Th2,process.Flag_hfPosFilter2Th4,process.Flag_hfNegFilter2Th4,process.Flag_hfPosFilter2Th5,process.Flag_hfNegFilter2Th5,process.Flag_hfCoincFilter2Th2,process.Flag_hfCoincFilter2Th3,process.Flag_hfCoincFilter2Th4,process.Flag_hfCoincFilter2Th5,process.Flag_hfPosFilter3,process.Flag_hfNegFilter3,process.Flag_hfPosFilter3Th2,process.Flag_hfNegFilter3Th2,process.Flag_hfPosFilter3Th4,process.Flag_hfNegFilter3Th4,process.Flag_hfPosFilter3Th5,process.Flag_hfNegFilter3Th5,process.Flag_hfCoincFilter3Th2,process.Flag_hfCoincFilter3Th3,process.Flag_hfCoincFilter3Th4,process.Flag_hfCoincFilter3Th5,process.Flag_hfPosFilter4,process.Flag_hfNegFilter4,process.Flag_hfPosFilter4Th2,process.Flag_hfNegFilter4Th2,process.Flag_hfPosFilter4Th4,process.Flag_hfNegFilter4Th4,process.Flag_hfPosFilter4Th5,process.Flag_hfNegFilter4Th5,process.Flag_hfCoincFilter4Th2,process.Flag_hfCoincFilter4Th3,process.Flag_hfCoincFilter4Th4,process.Flag_hfCoincFilter4Th5,process.Flag_hfPosFilter5,process.Flag_hfNegFilter5,process.Flag_hfPosFilter5Th2,process.Flag_hfNegFilter5Th2,process.Flag_hfPosFilter5Th4,process.Flag_hfNegFilter5Th4,process.Flag_hfPosFilter5Th5,process.Flag_hfNegFilter5Th5,process.Flag_hfCoincFilter5Th2,process.Flag_hfCoincFilter5Th3,process.Flag_hfCoincFilter5Th4,process.Flag_hfCoincFilter5Th5,process.endjob_step,process.MINIAODSIMoutput_step)
+
 process.schedule.associate(process.patTask)
 from PhysicsTools.PatAlgos.tools.helpers import associatePatAlgosToolsTask
 associatePatAlgosToolsTask(process)
