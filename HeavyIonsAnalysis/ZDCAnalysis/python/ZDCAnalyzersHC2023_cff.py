02faca7e5dd7f5216f8274fd81ef787171cda56e
boundino <boundino@gmail.com>
2026-01-30 07:01:31 +0100

    input explicit parameters in ZDCAnalyzersHC2023_cff

--- HeavyIonsAnalysis/ZDCAnalysis/python/ZDCAnalyzersHC2023_cff.py
+++ HeavyIonsAnalysis/ZDCAnalysis/python/ZDCAnalyzersHC2023_cff.py
@@ -1,11 +1,11 @@
 import FWCore.ParameterSet.Config as cms
 
 from HeavyIonsAnalysis.ZDCAnalysis.zdcreco2023_cfi import *
+zdcreco2023HardCode.zdcDigiSrc = cms.InputTag('hcalDigis', 'ZDC')
 zdcreco2023HardCode.calZDCDigi = False
-# zdcreco2023HardCode.skipRPD = True
+zdcreco2023HardCode.skipRPD = True
 from HeavyIonsAnalysis.ZDCAnalysis.zdcanalyzer_cfi import *
 zdcanalyzer.zdcRecHitSrc = cms.InputTag('zdcreco2023HardCode')
-zdcanalyzer.nZdcTs = cms.int32(6)
 zdcanalyzer.doZDCDigi = True
 zdcanalyzer.doZDCRecHit = True
 zdcanalyzer.calZDCDigi = False
